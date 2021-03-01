49a98b59d94441a71fdc9cf6bc455dd05260970f
sergejgold <35975766+sergejgold@users.noreply.github.com>
2021-03-01 22:11:21 +0300

    Update Stream_lesson1_3.java

--- Stream_lesson1_3.java
+++ Stream_lesson1_3.java
@@ -2,11 +2,10 @@ package com.stream.Person;
 import java.util.*;
 import java.util.stream.Collectors;
 
-public class Persons {
+public class Stream_lesson1_3 {
     public static List<String> sequence(String str) {
         List<String> str1 = Arrays.stream(str.split("-"))
-                .filter(x -> x.length() > 5)
-                .filter(x -> x.startsWith("a"))
+                .filter(x -> x.length() > 5 && x.startsWith("a"))
                 .collect(Collectors.toList());
         str1.forEach(System.out::println);
         return str1;
